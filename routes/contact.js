f459971b018f40630d3ef0699540ebfb9da25fa8
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2024-01-11 22:46:37 -0500

    Removed everything

--- routes/contact.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-/* GET contact page. */
-router.get("/", function (req, res, next) {
-  res.render("contact", { title: "Contact Us" });
-});
-module.exports = router;
--- routes/help.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-/* GET help page. */
-router.get("/", function (req, res, next) {
-  res.render("help", { title: "Help Solarity" });
-});
-module.exports = router;
